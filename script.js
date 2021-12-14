eb15b2afdc20e449b72fd16877d762ce29e51de5
choiwh000 <cwh915@naver.com>
2021-12-15 00:34:52 +0900

    JS Update

--- script.js
+++ script.js
@@ -16,15 +16,33 @@ $('.fa-times').click(function() {
 let a = 0
 
 
+// $('.t-eight').click(function() {
 
+//     if (a == 0) {
+//         $('#hiddenContent').fadeOut(500);
+//         a = 1
+//     }else if (a == 1) {
+//         $('#hiddenContent').fadeIn(500);
+//         a = 0
+//     }
+
+
+   
+// })
 
 $('.t-eight').click(function() {
 
     if (a == 0) {
-        $('#hiddenContent').hide(400);
+        // $('#hiddenContent').css('opacity','0');
+        $("#hiddenContent").animate({opacity: "0"},300);
+        // $('.designMajor').css('margin-top','-676px');
+        $(".designMajor").animate({marginTop: "-676px"},500);
         a = 1
     }else if (a == 1) {
-        $('#hiddenContent').show(400);
+        // $('#hiddenContent').css('opacity','1');
+        $("#hiddenContent").animate({opacity: "1"},1000);
+        // $('.designMajor').css('margin-top','166px');
+        $(".designMajor").animate({marginTop: "166px"},500);
         a = 0
     }
 
